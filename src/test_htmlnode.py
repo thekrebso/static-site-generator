3d2cff5ddf2d6929f16c6baad1f59b8a20109230
krebso <krebso@krebso.pl>
2025-05-19 03:53:21 +0200

    added LeafNode tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -36,6 +36,48 @@ class TestHTMLNode(unittest.TestCase):
         html_props = node.props_to_html()
 
         self.assertEqual(html_props, ' href="http://localhost" target="_blank"')
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html1(self):
+        leafNode = LeafNode(None, None) # type: ignore
+        self.assertRaises(ValueError, leafNode.to_html)
+
+    def test_to_html2(self):
+        leafNode = LeafNode("p", None) # type: ignore
+        self.assertRaises(ValueError, leafNode.to_html)
+
+    def test_to_html3(self):
+        leafNode = LeafNode("p", "")
+        self.assertRaises(ValueError, leafNode.to_html)
+
+    def test_to_html4(self):
+        leafNode = LeafNode(None, "This is a leaf node without tag")
+        self.assertEqual(
+            leafNode.to_html(),
+            "This is a leaf node without tag"
+        )
+
+    def test_to_html5(self):
+        leafNode = LeafNode(None, "This is a leaf node without tag but with props!?", { "color": "red" })
+        self.assertEqual(
+            leafNode.to_html(),
+            "This is a leaf node without tag but with props!?"
+        )
+
+    def test_to_html6(self):
+        leafNode = LeafNode("p", "This is a leaf node in p tag")
+        self.assertEqual(
+            leafNode.to_html(),
+            "<p>This is a leaf node in p tag</p>"
+        )
+
+    def test_to_html7(self):
+        leafNode = LeafNode("p", "This is a leaf node in p tag with props", { "color": "red", "background-color": "blue" })
+        self.assertEqual(
+            leafNode.to_html(),
+            '<p color="red" background-color="blue">This is a leaf node in p tag with props</p>'
+        )
 
 
 if __name__ == "__main__":
