efe5a8a7f219a45bef5736bf6b0a9dbcf63c5b97
krebso <krebso@krebso.pl>
2025-05-19 03:10:16 +0200

    checking __repr__ output

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 
 def main():
-    textNode = TextNode("Some anchor text", TextType.LINK, "https://github.com")
+    textNode1 = TextNode("Some bold text", TextType.BOLD)
+    textNode2 = TextNode("Some anchor text", TextType.LINK, "https://github.com")
+    htmlNode1 = HTMLNode(None, None, None, None)
+    htmlNode2 = HTMLNode("h1", "this is a text inside a tag", [htmlNode1], { "target": "_blank" })
 
-    print(textNode)
+    print(textNode1)
+    print(textNode2)
+    print(htmlNode1)
+    print(htmlNode2)
 
 
 if __name__ == "__main__":
