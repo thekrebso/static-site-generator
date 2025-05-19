b6557e72f0d01b6ba28db2190d9036274547f61a
krebso <krebso@krebso.pl>
2025-05-20 00:33:41 +0200

    more descriptive function names

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,21 +4,21 @@ from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_props_to_html1(self):
+    def test_props_to_html_shouldWorkWithoutProps1(self):
         node = HTMLNode(None, None, None, None)
         
         html_props = node.props_to_html()
         
         self.assertEqual(html_props, '')
 
-    def test_props_to_html2(self):
+    def test_props_to_html_shouldWorkWithoutProps2(self):
         node = HTMLNode(None, None, None, {})
         
         html_props = node.props_to_html()
         
         self.assertEqual(html_props, '')
 
-    def test_props_to_html3(self):
+    def test_props_to_html_shouldWorkWithProp(self):
         node = HTMLNode(None, None, None, {
             "href": "http://localhost",
         })
@@ -27,7 +27,7 @@ class TestHTMLNode(unittest.TestCase):
 
         self.assertEqual(html_props, ' href="http://localhost"')
 
-    def test_props_to_html4(self):
+    def test_props_to_html_shouldWorkWithMultipleProps(self):
         node = HTMLNode(None, None, None, {
             "href": "http://localhost",
             "target": "_blank"
@@ -39,40 +39,40 @@ class TestHTMLNode(unittest.TestCase):
 
 
 class TestLeafNode(unittest.TestCase):
-    def test_to_html1(self):
+    def test_to_html_shouldRaiseOnInvalidValue1(self):
         leafNode = LeafNode(None, None) # type: ignore
         self.assertRaises(ValueError, leafNode.to_html)
 
-    def test_to_html2(self):
+    def test_to_html_shouldRaiseOnInvalidValue2(self):
         leafNode = LeafNode("p", None) # type: ignore
         self.assertRaises(ValueError, leafNode.to_html)
 
-    def test_to_html3(self):
+    def test_to_html_shouldRaiseOnInvalidValue3(self):
         leafNode = LeafNode("p", "")
         self.assertRaises(ValueError, leafNode.to_html)
 
-    def test_to_html4(self):
+    def test_to_html_shouldWorkWithoutATag(self):
         leafNode = LeafNode(None, "This is a leaf node without tag")
         self.assertEqual(
             leafNode.to_html(),
             "This is a leaf node without tag"
         )
 
-    def test_to_html5(self):
+    def test_to_html_shouldWorkWithoutATagWithProps(self):
         leafNode = LeafNode(None, "This is a leaf node without tag but with props!?", { "color": "red" })
         self.assertEqual(
             leafNode.to_html(),
             "This is a leaf node without tag but with props!?"
         )
 
-    def test_to_html6(self):
+    def test_to_html_shouldWorkWithTag(self):
         leafNode = LeafNode("p", "This is a leaf node in p tag")
         self.assertEqual(
             leafNode.to_html(),
             "<p>This is a leaf node in p tag</p>"
         )
 
-    def test_to_html7(self):
+    def test_to_html_shouldWorkWithTagWithProps(self):
         leafNode = LeafNode("p", "This is a leaf node in p tag with props", { "color": "red", "background-color": "blue" })
         self.assertEqual(
             leafNode.to_html(),
