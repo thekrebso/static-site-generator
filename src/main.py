88adc1cde6a5cc32e3dbbcfebf502e9671389424
krebso <krebso@krebso.pl>
2025-05-20 00:56:48 +0200

    add TextNode to HTMLNode conversion function

--- src/main.py
+++ src/main.py
@@ -2,6 +2,30 @@ from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode
 
 
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            if isinstance(text_node.url, str):
+                return LeafNode("a", text_node.text, { "href": text_node.url })
+            else:
+                return LeafNode("a", text_node.text, { "href": "" })
+        case TextType.IMAGE:
+            if isinstance(text_node.url, str):
+                return LeafNode("img", "", { "src": text_node.url, "alt": text_node.text })
+            else:
+                return LeafNode("img", "", { "src": "", "alt": text_node.text })
+        case _:
+            raise Exception("invalid TextType value")
+
+
 def main():
     textNode1 = TextNode("Some bold text", TextType.BOLD)
     textNode2 = TextNode("Some anchor text", TextType.LINK, "https://github.com")
