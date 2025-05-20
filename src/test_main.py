060e99ffa3b21445373e9c74057e822ca1f0292b
krebso <krebso@krebso.pl>
2025-05-21 01:19:23 +0200

    added message

--- src/test_main.py
+++ src/test_main.py
@@ -60,15 +60,18 @@ class Test_TextNode_to_HTMLNode(unittest.TestCase):
 class Test_split_TextNode(unittest.TestCase):
     def test_works(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+
+        actual = split_nodes_delimiter([node], "`", TextType.CODE)
 
         self.assertEqual(
-            new_nodes,
-            [
-                TextNode("This is text with a ", TextType.TEXT),
-                TextNode("code block", TextType.CODE),
-                TextNode(" word", TextType.TEXT),
-            ]
+            actual,
+            expected,
+            f"\nExpected: {expected}\nActual: {actual}"
         )
 
 
