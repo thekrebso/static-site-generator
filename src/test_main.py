dd249d44d182c366ae1eb034e695f7b5ede28aa2
krebso <krebso@krebso.pl>
2025-05-21 23:12:56 +0200

    added initial tests for split_nodes_image and split_nodes_link

--- src/test_main.py
+++ src/test_main.py
@@ -1,7 +1,14 @@
 import unittest
 
 from textnode import TextType, TextNode
-from main import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from main import (
+    text_node_to_html_node, 
+    split_nodes_delimiter, 
+    extract_markdown_images, 
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link
+)
 
 
 class Test_TextNode_to_HTMLNode(unittest.TestCase):
@@ -57,7 +64,7 @@ class Test_TextNode_to_HTMLNode(unittest.TestCase):
         self.assertRaises(Exception, text_node_to_html_node, node)
 
 
-class Test_split_TextNode(unittest.TestCase):
+class Test_split_nodes_delimiter(unittest.TestCase):
     def test_inline_code(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         expected = [
@@ -203,6 +210,46 @@ class Test_extract_md_links(unittest.TestCase):
 
         self.assertEqual(actual, expected, 
                          f"\nExpected: {expected}\nActual: {actual}")
+
+
+class Test_split_nodes_image(unittest.TestCase):
+    def test_works(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        expected = [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode(
+                "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+            ),
+        ]
+
+        actual = split_nodes_image([node])
+
+        self.assertListEqual(actual, expected)
+
+
+class Test_split_nodes_link(unittest.TestCase):
+    def test_works(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        expected = [
+            TextNode("This is text with a link ", TextType.TEXT),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode(
+                "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+            ),
+        ]
+
+        actual = split_nodes_link([node])
+
+        self.assertListEqual(actual, expected)
 
 
 if __name__ == "__main__":
