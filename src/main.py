43e280099c16b474a168993999ecc4327885bed5
krebso <krebso@krebso.pl>
2025-05-22 00:02:24 +0200

    fixed return type

--- src/main.py
+++ src/main.py
@@ -50,12 +50,12 @@ def split_nodes_delimiter(old_nodes: list['TextNode'], delimiter: str, text_type
     return new_nodes
 
 
-def extract_markdown_images(text: str) -> Optional[list[tuple[str, str]]]:
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
     matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
 
 
-def extract_markdown_links(text: str) -> Optional[list[tuple[str, str]]]:
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
 
