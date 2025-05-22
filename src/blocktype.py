b5fd18e91c053c0150a6b0516b03b4e030a86a9d
krebso <krebso@krebso.pl>
2025-05-22 22:07:51 +0200

    added BlockType enum and empty block_to_blocktype function

---
+++ src/blocktype.py
@@ -0,0 +1,15 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+
+def block_to_blocktype(block: str) -> 'BlockType':
+    pass
