ed97629fb827f37cd1fc3210c8c17d6f9a3852d4
krebso <krebso@krebso.pl>
2025-05-22 22:38:18 +0200

    added simple block_to_blocktype implementation

--- src/blocktype.py
+++ src/blocktype.py
@@ -11,5 +11,26 @@ class BlockType(Enum):
     ORDERED_LIST = "ordered list"
 
 
+def is_ordered_list(text: str) -> bool:
+    lines = text.splitlines()
+    
+    for i, line in enumerate(lines):
+        if not line.startswith(f"{i+1}. "):
+            return False
+
+    return True
+
+
 def block_to_blocktype(block: str) -> 'BlockType':
-    pass
+    if bool(re.match(r"^#{1,6} (?!#).*", block)):
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    elif all(line.startswith("- ") for line in block.splitlines()):
+        return BlockType.UNORDERED_LIST
+    elif is_ordered_list(block):
+        return BlockType.ORDERED_LIST
+    elif all(line.startswith("> ") for line in block.splitlines()):
+        return BlockType.QUOTE
+    else:
+        return BlockType.PARAGRAPH
